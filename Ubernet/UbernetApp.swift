b35672db1c8bfac816bc225292a650d32722fa7a
Fabio Fiorita <fabiolfp@gmail.com>
2022-05-24 11:20:30 -0300

    Initial Commit

---
+++ Ubernet/UbernetApp.swift
@@ -0,0 +1,17 @@
+//
+//  UbernetApp.swift
+//  Ubernet
+//
+//  Created by Fabio Fiorita on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct UbernetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
